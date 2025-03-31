960199d3970cd1896fc1a975254fe1aba1683d33
juliatarasow <72554413+juliatarasow@users.noreply.github.com>
2025-03-31 14:01:23 +0200

    Projektdateien hinzufügen.

---
+++ Controllers/JobPostingController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SQLitePCL;
+using Worktastic.Data;
+using Worktastic.Models;
+
+namespace Worktastic.Controllers
+{
+    public class JobPostingController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public JobPostingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var jobPostingsFromDb = _context.JobPostings.Where(x=> x.OwnerUsername);
+            return View();
+        }
+        public IActionResult CreatedEditJobPosting(int id)
+        {
+            return View();
+        }
+        public IActionResult CreateEditJob(JobPostingModel jobPostingModel, IFormFile file)
+        {
+            //beim Speichern
+            jobPostingModel.OwnerUsername = User.Identity.Name;
+
+            if (file != null)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    file.CopyTo(ms);
+                    var bytes = ms.ToArray();
+                    jobPostingModel.CompanyImage = bytes;
+                }
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/JobPostingModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Worktastic.Models
+{
+    public class JobPostingModel
+    {
+        public int Id { get; set; }
+        public string JobTitle { get; set; }
+        public string JobDescription { get; set; }
+        public string JobLocation { get; set; }
+        public DateTime StartDate { get; set; }
+        public int Salary { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string ContactMail { get; set; }
+        public string CompanyWebsite { get; set; }
+        public byte[] CompanyImage { get; set; }
+        public string OwnerUsername { get; set; }
+    }
+}
