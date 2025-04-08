cfca9fe5090d02192cae7a0ecaacf79bedbe4bc7
juliatarasow <72554413+juliatarasow@users.noreply.github.com>
2025-04-08 09:14:36 +0200

    JS- und CSS-Datei eingebunden

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,54 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    console.log("JavaScript und jQuery geladen.");
 
-// Write your JavaScript code.
+    $('#adminTable').DataTable({
+        pagingType: "full_numbers",
+        language: {
+            search: "Suche:",
+            lengthMenu: "Zeige _MENU_ Einträge",
+            info: "Zeige _START_ bis _END_ von _TOTAL_ Einträgen",
+            paginate: {
+                first: "Erste",
+                last: "Letzte",
+                next: "Nächste",
+                previous: "Vorherige"
+            }
+        },
+        responsive: true,
+        paging: true,
+        lengthChange: true,
+        searching: true,
+        ordering: true,
+        info: true,
+        autoWidth: false
+    });
+
+    // Funktion global verfügbar machen
+    window.deleteJobPosting = function (jobPostingId) {
+        Swal.fire({
+            title: "Wollen Sie diesen Eintrag wirklich löschen?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Ja, löschen",
+            cancelButtonText: "Abbrechen",
+            confirmButtonColor: "#dc3545",
+            cancelButtonColor: "#6c757d"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    type: "POST",
+                    url: deleteJobPostingUrl,
+                    data: { id: jobPostingId },
+                    success: function () {
+                        Swal.fire("Eintrag gelöscht", "", "success").then(() => {
+                            location.reload();
+                        });
+                    },
+                    error: function () {
+                        Swal.fire("Fehler beim Löschen", "Bitte versuchen Sie es erneut.", "error");
+                    }
+                });
+            }
+        });
+    };
+});
